05a218b9830879c8d81f5f8da1ac43492d1b2eca
root <root@localhost>
2024-10-27 10:00:44 +0530

    auto publish 1730003444058

--- src/main.py
+++ src/main.py
@@ -25,45 +25,81 @@ if age > 3:
 if age <3:
   print(f"Ohh... you are {3-age} younger than me")
 
-game = input("Want to play games!  ").lower()
 while True:
-  if "yes" in game and "no" in game:
-    game = input("i didn't understand yes or no?  ")
-  if "no" in game and "yes" not in game:
-   pass
-  elif "yes" in game:
+  game =  input("Want to play games!  ").lower()
+  if "yes" in game:
     print("I have games to play")
-    print("spelling checker")
+    print("stone paper scissors")
     print("Random number")
-   
+    
     while True:
-      gam = input("which one you want to play(r for Random number and spt for other) ").lower()
-      gam = gam.lower()
-      if "r" not in gam and "spt" not in gam:
-        print("type r or s to play")
-      
-       
-       
-      if gam == "r":
-         print("so choose a number between 1 to 10 and if the number is the number that I am thinking than you will win")
-         c_no =  random.randint(1,10)
-         no = input("choose  ")
-         no = int(no)
-         if no == c_no:
-           print("Wow you win!")
-         if no != c_no:
-           print(f"ohh... you lost better luck next time, well the no. is {c_no}")
-      break
-      if gam == "s":
-         print("so in this i will give you two spellings and you have to which one is correct")
-         cor = input(" break or braek tell,tell  ")
-         if cor == "break":
-            print("yayy! you win!")
-         if cor == "braek":
-            print("sorry'but better luck next time")
+       gam = input("which one you want to play(r for Random number and spt for other) ").lower()
+       if "r" not in gam and "spt" not in gam:
+         print("type r or spt to play")
+       if "r" in game or "spt" in game:
          break
-  else:
-    game = input("bro type something like yes or no.  ")
+       
+       
+       if "r" in gam:
+         while True:
+       
+            print("so choose a number between 1 to 10 and if the number is the number that I am thinking than you will win")
+            c_no =  random.randint(1,10)
+            while True:
+              no = input("choose  ")
+              try:
+                no = int(no)
+                break
+              except ValueError:
+                print("bro type a number")
+            
+            
+            if no == c_no:
+              print("Wow you win!")
+            if no != c_no:
+              print(f"ohh... you lost better luck next time, well the no. is {c_no}")
+            retry = input("Want to play again?  ")
+            if "no" in retry:
+              break
+            if "yes" in retry:
+              continue
+            if "yes" in retry and "no" in retry or "yes" not in retry and "no" not in retry:
+              print("Bro try to type yes or no")
+            
+      
+      
+      
+       if "spt" in gam:
+       print("self explanatory stone paper scissors, the legendry game of choice in which Rock crushes scissors, scissors cuts paper, and paper covers rock. If both players make the same decision, it's a tie. ")
+       while True:
+         
+          cor = input(" choice s for scissors, p for paper and st for stone  ")
+          c_c = random.choice(["st","p","s"])
+          if cor == c_c:
+             print("it's a tie")
+          if cor != c_c:
+             print("sorry'but better luck next time")
+          if cor != "st" or "p" or "s":
+            print("bro type st for stone, p for paper and s for scissors")
+          
+            continue 
+          
+       break
+       
+
+
+
+  if "yes" in game and "no" in game:
+      game = input("i didn't understand yes or no?  ")
+  
+  if "no" in game and "yes" not in game:
+      break
+  if "yes" not in game and "no" not in game:
+      print("bro type something like yes or no.  ")
+  
+  
+  
+
   
     
 print("I hope you enjoyed but its time to say bye")
